f0eb8e72ef92ca0096f878dc273d27a019eb2305
Mes Morts <titouanescorneboueu@gmail.com>
2021-06-07 14:56:33 +0200

    Fin de la home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,13 @@
 import NavBar from "../components/NavBar"
-import { motion } from "framer-motion"
 
 const HomePage = () => {
     return (
         <main className = "home_page">
             <div className="home_page_top">
                 <h1>Titouan Escorneboueü</h1>
-                <motion.div whileHover={{}}>
-                    <div className="conteneur_dev_web">
-                        <h2>Developpeur web / web mobile</h2>
-                    </div>
-                </motion.div>
+                <div className="conteneur_dev_web">
+                    <h2>Developpeur web / web mobile</h2>
+                </div>
             </div>
             <NavBar/>
         </main>
