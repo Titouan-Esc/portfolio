16015ea99c4f678bc6b33d7cdec2d436d78e90f3
Mes Morts <titouanescorneboueu@gmail.com>
2021-06-08 11:11:24 +0200

    je vais aller manger

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,46 @@
 import { Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import { jsPDF } from 'jspdf';
+import Pdf from '../assets/image/CV Titouan Escorneboueu-1.png';
 
 
 const Contact = () => {
+
+    const pdfGenerator = () => {
+        const doc = new jsPDF();
+        doc.addImage(Pdf, 'PNG', 65, 20, 500, 400);
+        doc.save('a.pdf')
+    }
+
+
     return (
         <main className="contact">
+
+            <div className="contact_top">
+                <h1>Contact</h1>
+                <NavBar/>
+            </div>
+
+            <div className="contact_text">
+                <p>Via mail à <span>titouanescorneboueu@gmail.com</span></p>
+                <p>Via téléphone au <span>06.51.76.06.25</span></p>
+                <p>Et sur tout les autres réseaux possible</p>
+            </div>
             
-            <Link to="/">Home Page</Link>
+            <div className="reseaux_cv">
+                <div className="reseaux">
+                    <h2>Situé sur Bordeaux</h2>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+                <div className="cv">
+                    <h2>Télécharger mon CV</h2>
+                    <button onClick={() => pdfGenerator()}>Télécharger</button>
+                </div>
+            </div>
+            <Link to="/" className="link_homepage">Home Page</Link>
         </main>
     )
 }
--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -1,12 +1,39 @@
 import { Link } from 'react-router-dom';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import NavBar from '../components/NavBar';
+
+const propriete = {
+    arrows: false,
+}
 
 
 const Projets = () => {
     return (
         <main className="projets">
-            
+            <div className="projets_top">
+                <h1>Projets</h1>
+                <NavBar/>
+            </div>
 
-            <Link to="/">Home Page</Link>
+            <Slide {...propriete}>
+                <div className="each-slide">
+                    <img src="" alt="" />
+                </div>
+                <div className="each-slide">
+                    <img src="" alt="" />
+                </div>
+                <div className="each-slide">
+                    <img src="" alt="" />
+                </div>
+                <div className="each-slide">
+                    <img src="" alt="" />
+                </div>
+                <div className="each-slide">
+                    <img src="" alt="" />
+                </div>
+            </Slide>
+            <Link to="/" className="link_homepage">Home Page</Link>
         </main>
     )
 }
