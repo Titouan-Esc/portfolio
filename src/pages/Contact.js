b5412efa87f6c0a5d82a007793c5e7ed62b3dff2
Mes Morts <titouanescorneboueu@gmail.com>
2021-06-08 16:02:32 +0200

    page contact fini

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,22 +2,26 @@ import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import { jsPDF } from 'jspdf';
 import Pdf from '../assets/image/CV Titouan Escorneboueu-1.png';
+import React, { useState } from 'react';
+
+
 
 
 const Contact = () => {
 
+    const [text, setText] = useState('');
+
     const pdfGenerator = () => {
         const doc = new jsPDF();
-        doc.addImage(Pdf, 'PNG', 65, 20, 500, 400);
-        doc.save('a.pdf')
+        doc.addImage(Pdf, 'PNG', 5, 0, 200, 300);
+        doc.save('CV.pdf')
     }
-
 
     return (
         <main className="contact">
 
             <div className="contact_top">
-                <h1>Contact</h1>
+                <h1>Me contacter</h1>
                 <NavBar/>
             </div>
 
@@ -30,10 +34,12 @@ const Contact = () => {
             <div className="reseaux_cv">
                 <div className="reseaux">
                     <h2>Situé sur Bordeaux</h2>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <div className="reseaux_logo">
+                        <a href="https://github.com/Titouan-Esc?tab=repositories"><img src="./image/github.svg" alt="GitHub" /></a>
+                        <a href="https://www.linkedin.com/in/titouan-escorneboueu/"><img src="./image/linkedin.svg" alt="LinKedin" /></a>
+                        <a href="https://www.instagram.com/don_oscar_anton/"><img src="./image/logo-instagram.svg" alt="Instagram" /></a>
+                        <a href="https://www.facebook.com/profile.php?id=100007820807943"><img src="./image/facebook.svg" alt="Facebook" /></a>
+                    </div>
                 </div>
                 <div className="cv">
                     <h2>Télécharger mon CV</h2>
